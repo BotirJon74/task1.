b9ee53708f40f79a7baf7d29b88cbc8eb5b1c380
BotirJon74 <31174@mail.ru>
2024-08-30 18:29:39 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,6 @@
 # 1st program
 print(9**0.5*5)
+
 # "2nd program"
 print(9.99>9.98 and 1000!=1000.1)
 
@@ -9,4 +10,3 @@ print(2*2+2==2*(2+2))
 # "4th program"
 print(float('123.456')*10)
 print(int(1234.56%10))
-
